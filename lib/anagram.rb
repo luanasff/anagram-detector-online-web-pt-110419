8be832c26a493536105c77434e6112d104750100
Luana Faria Lane <luanasfff@gmail.com>
2019-12-17 01:46:38 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+
+
+class Anagram
+  attr_accessor :word 
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(list)
+    list.select { |v| v.split("").sort == @word.split("").sort }
+  end
+end
+
